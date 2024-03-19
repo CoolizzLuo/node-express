546501fe7ddcf57d6e4833683c6290ed181b4685
enzo.luo <duenzo1010@gmail.com>
2024-03-19 08:58:15 +0800

    build: init project

---
+++ src/app.ts
@@ -0,0 +1,19 @@
+import 'dotenv/config';
+import express from 'express';
+import router from './routes';
+
+const app = express();
+const port = process.env.PORT ?? 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use('/auth', router.authRouter);
+
+app.get('/', (req, res) => {
+  res.send('Hello World!!');
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
